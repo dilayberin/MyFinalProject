5847a10475c7af79a311bd844ffd5b1e4b5cc248
Dilay Berin Akkan <121443116+dilayberin@users.noreply.github.com>
2025-02-19 17:39:36 +0300

    Revert "Main"

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // ProductTest();
+           CategoryManager categoryManager=new CategoryManager(new EFCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+
+
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EFProductDal());
+
+            foreach (var product in productManager.GetAllByCategoryId(2))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
+
