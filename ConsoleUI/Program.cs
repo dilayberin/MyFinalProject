5828ea98daa9387a19123a6312cf3e6fabe2dd85
Dilay Berin Akkan <dilayakkan20@gmail.com>
2025-02-20 17:00:45 +0300

    20.02.05

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -8,23 +8,27 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-           // ProductTest();
-           CategoryManager categoryManager=new CategoryManager(new EFCategoryDal());
+            ProductTest();
+            //CategoryTest();
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EFCategoryDal());
             foreach (var category in categoryManager.GetAll())
             {
                 Console.WriteLine(category.CategoryName);
             }
-
-
         }
 
         private static void ProductTest()
         {
             ProductManager productManager = new ProductManager(new EFProductDal());
 
-            foreach (var product in productManager.GetAllByCategoryId(2))
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName+"/"+product.CategoryName);
             }
         }
     }
